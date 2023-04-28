c7095c58c3179e6f0ebfca62c236af12a6575db3
Soumya <arsoumya21@gmail.com>
2023-04-28 21:03:34 +0530

    third commit

--- miniproject/recipe/views.py
+++ miniproject/recipe/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
-    print('HELLLLLLLLLLLLLLLLLLLLLLLLLLLO')
     return render(request, 'index.html')
 
 def result(request):
     if request.method == 'POST':
-        print(request.POST)
         selected_checkboxes = []
         checkbox_names = ['potato', 'cauli', 'onion']
         for checkbox_name in checkbox_names:
@@ -14,11 +12,14 @@ def result(request):
                 selected_checkboxes.append(checkbox_name)
         
         # Perform any additional processing with the selected checkboxes
-        
+       
+        recipetext = " " #recipe should be in this variable
+        new_recipetext = recipetext.replace('Step', '\nStep')
+ 
         context = {
-            'selected_checkboxes': selected_checkboxes
+            'selected_checkboxes': selected_checkboxes,
+            'recipetext':new_recipetext
         }
-        
         return render(request, 'result.html', context)
     
     return render(request, 'test.html')
