ffd8a439452bf97018f02167d64142ee03acf08a
Soumya <arsoumya21@gmail.com>
2023-04-29 12:02:27 +0530

    fixed buttons

--- miniproject/recipe/views.py
+++ miniproject/recipe/views.py
@@ -7,10 +7,10 @@ def index(request):
     return render(request, 'index.html')
 
 def result(request):
-    # myclient = pymongo.MongoClient("localhost", 27017)
-    # mydb = myclient["recipeDB"]
-    # col1 = mydb["Categories"]
-    # col2 = mydb["Recipes"]
+    myclient = pymongo.MongoClient("localhost", 27017)
+    mydb = myclient["recipeDB"]
+    col1 = mydb["Categories"]
+    col2 = mydb["Recipes"]
     if request.method == 'POST':
         print(request.POST)
         selected_checkboxes = []
@@ -19,18 +19,14 @@ def result(request):
             if checkbox_name in request.POST:
                 selected_checkboxes.append(checkbox_name)
         
-        # Perform any additional processing with the selected checkboxes
         recipes = []
         for x in col2.find({'Ingredients':{'$all':selected_checkboxes}}):
             r = {'title':x['Title'], 'img':x['Image'],'det':x['Recipe']}
-            #x['Recipe'].insert(0, x['Title'])
             recipes.append(r)
-        #recipetext = " " #recipe should be in this variable
-        #new_recipetext = recipetext.replace('Step', '\nStep')
  
         context = {
             'selected_checkboxes': selected_checkboxes,
-            'recipes' : recipetext
+            'recipes' : recipes
         }
         return render(request, 'result.html', context)
     
