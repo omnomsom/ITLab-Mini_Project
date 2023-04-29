73ba58e0fe9d0b8a7feba5d2fe227895401cf0f3
Soumya <arsoumya21@gmail.com>
2023-04-29 10:08:07 +0530

    design changes

--- miniproject/recipe/views.py
+++ miniproject/recipe/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 import pymongo
-from recipe.models import Recipe, RecipeForm
-from django.http import HttpResponseRedirect
 
 def index(request):
     return render(request, 'index.html')
@@ -14,7 +12,7 @@ def result(request):
     if request.method == 'POST':
         print(request.POST)
         selected_checkboxes = []
-        checkbox_names = ['Potato', 'Cabbage', 'Onions', 'Tomato', 'French Beans', 'Beetroot', 'Garlic', 'Basmati Rice', 'Paneer', 'Curd', 'Chicken', 'Green Chilli', 'Cream', 'Butter', 'Fruits', 'Capsicum', 'Bread', 'Lemon', 'Maida', 'Cheese']
+        checkbox_names = ['Potato', 'Cabbage', 'Onions', 'Tomato', 'French Beans', 'Beetroot', 'Garlic', 'Rice', 'Paneer', 'Curd', 'Chicken', 'Green Chillies', 'Cream', 'Butter', 'Fruits', 'Capsicum', 'Bread', 'Lemon', 'Maida', 'Cheese']
         for checkbox_name in checkbox_names:
             if checkbox_name in request.POST:
                 selected_checkboxes.append(checkbox_name)
@@ -22,9 +20,8 @@ def result(request):
         # Perform any additional processing with the selected checkboxes
         recipes = []
         for x in col2.find({'Ingredients':{'$all':selected_checkboxes}}):
-            r = {'title':x['Title'], 'img':x['Image'],'det':x['Recipe']}
-            #x['Recipe'].insert(0, x['Title'])
-            recipes.append(r)
+            x['Recipe'].insert(0, x['Title'])
+            recipes.append(x['Recipe'])
         #recipetext = " " #recipe should be in this variable
         #new_recipetext = recipetext.replace('Step', '\nStep')
  
@@ -35,16 +32,3 @@ def result(request):
         return render(request, 'result.html', context)
     
     return render(request, 'test.html')
-
-def add(request):
-  entries = Recipe.objects.all()[:10]
-  return render(request, 'add.html', {'entries':entries,'form':RecipeForm()})
-
-def add_recipe(request):
-   if request.method == 'POST':
-      form = RecipeForm(request.POST)
-      if form.is_valid():
-        entry = form.save()
-   return HttpResponseRedirect('/')
-
-
